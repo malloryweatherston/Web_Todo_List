e4a180ee63634407d40e3a2017fffad00ae6009b
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-29 14:40:02 -0500

    added function to save file and post new items

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,7 +1,10 @@
 <?php
 
 $items = [];
- $filename = 'data/list.txt'; 
+//load contents of file
+
+
+$filename = 'data/list.txt'; 
 
 // function get_input($upper = false) { 
 //     $result = trim(fgets(STDIN)); 
@@ -9,66 +12,57 @@ $items = [];
 // } 
 
 
-function add_file($items) {
+function add_file($filename) {
      //echo "Enter the file path you want to add: \n";
     $items = [];
     $filename = 'data/list.txt';
     $filesize = filesize($filename);
     $read = fopen($filename, "r"); 
     $string_list = trim(fread($read, $filesize));
-    $list_array = explode("\n", $string_list);
-    $output = array_merge($items, $list_array);
+    $items = explode("\n", $string_list);
     fclose($read);
-    return ($output);
+    return $items;
 }
-
-function save_file($items) {
-    $filename = 'data/list.txt';
-    if (!file_exists($filename)) {
-        $handle = fopen($filename, 'w');
-    }
-        foreach ($items as $item) {
-            fwrite($handle, $item . PHP_EOL);
-        } 
-            fclose($handle);
-}
-
 
 $items = add_file($filename);
 
+function save_file($items) {
+    $filename = 'data/list.txt';
+    $handle = fopen($filename, 'w');
+    foreach ($items as $item) {
+        fwrite($handle, $item . PHP_EOL);
+    }
+    fclose($handle);
+}
 
+	
+if(isset($_POST['Add_Item'])){
+	$newTodo = $_POST['Add_Item'];
+	$items[] = $newTodo; 
+}
 
+save_file($items); 
 
 ?>
 
 
 <!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<title>TODO List</title>
-		</head>
-			<body>
-				<h2>TODO List</h2>
-					<ul>
-					<?php 
-						$list_array = ['Go to the gym', 'Pick up laundry', 'Cook dinner']; 
-							$items = add_file($filename);
-							$array = array_merge($items, $list_array);
-							foreach($array as $arrays) {
-								echo "<li>$arrays</li>" . PHP_EOL;
-							}
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>TODO List</title>
+	</head>
+	<body>
+		<h2>TODO List</h2>
+		<ul>
+			<?php 
+				foreach($items as $item) {
+					echo "<li>$item</li>" . PHP_EOL;
+				}
+			?>
+		</ul>
 
-
-					?>
-					</ul>
-
-				<h3>Add an Item to the TODO List</h3>
-				<?php 
-					foreach($_POST as $key => $value) {
-						echo "<p>{$key} => {$value}</p>";
-					}
-				?>
+		<h3>Add an Item to the TODO List</h3>
 
 					<form method="POST">
 						<p>
@@ -76,12 +70,8 @@ $items = add_file($filename);
 							<input id="Add_Item" name="Add_Item" type="text" placeholder="Enter New Item Here"> 
 							
 							<?php
-        					var_dump($_POST);
-
-
-
-    						?>
-
+        						var_dump($_POST);
+							?>
 						</p>
 						<p>
 						 <button type="Submit">Add</button>
