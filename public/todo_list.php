e18d68b2c8a23e61a11440076912171fd2448921
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-29 13:25:47 -0500

    Created Function to add file to array

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,3 +1,47 @@
+<?php
+
+$items = [];
+ $filename = 'data/list.txt'; 
+
+// function get_input($upper = false) { 
+//     $result = trim(fgets(STDIN)); 
+//     return $upper ? strtoupper($result) : $result;
+// } 
+
+
+function add_file($items) {
+     //echo "Enter the file path you want to add: \n";
+    $items = [];
+    $filename = 'data/list.txt';
+    $filesize = filesize($filename);
+    $read = fopen($filename, "r"); 
+    $string_list = trim(fread($read, $filesize));
+    $list_array = explode("\n", $string_list);
+    $output = array_merge($items, $list_array);
+    fclose($read);
+    return ($output);
+}
+
+function save_file($items) {
+    $filename = 'data/list.txt';
+    if (!file_exists($filename)) {
+        $handle = fopen($filename, 'w');
+    }
+        foreach ($items as $item) {
+            fwrite($handle, $item . PHP_EOL);
+        } 
+            fclose($handle);
+}
+
+
+$items = add_file($filename);
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 	<html>
 		<head>
@@ -7,21 +51,37 @@
 			<body>
 				<h2>TODO List</h2>
 					<ul>
-					<li>Do laundry</li>
-					<li>Walk dog</li>
-					<li>Go to the store</li>
-					<li>Do Homework</li>
-					<li>Go to the gym</li>
-				</ul>
+					<?php 
+						$list_array = ['Go to the gym', 'Pick up laundry', 'Cook dinner']; 
+							$items = add_file($filename);
+							$array = array_merge($items, $list_array);
+							foreach($array as $arrays) {
+								echo "<li>$arrays</li>" . PHP_EOL;
+							}
+
+
+					?>
+					</ul>
+
 				<h3>Add an Item to the TODO List</h3>
+				<?php 
+					foreach($_POST as $key => $value) {
+						echo "<p>{$key} => {$value}</p>";
+					}
+				?>
+
 					<form method="POST">
-						<?php
-        				var_dump($_GET);
-        				var_dump($_POST);
-    					?>
 						<p>
 							<label for="Add_Item">Item:</label>
 							<input id="Add_Item" name="Add_Item" type="text" placeholder="Enter New Item Here"> 
+							
+							<?php
+        					var_dump($_POST);
+
+
+
+    						?>
+
 						</p>
 						<p>
 						 <button type="Submit">Add</button>
